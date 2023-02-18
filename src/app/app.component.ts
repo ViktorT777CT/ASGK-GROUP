9e17434cf46f48ff073ea4d7071ac32f82981903
ViktorT777CT <p_viktor91@mail.ru>
2023-02-18 23:41:20 +0600

    сделал получение токена

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {AuthService} from './services/auth.service';
-import {HttpClient} from '@angular/common/http';
 
 
 @Component({
@@ -10,16 +8,5 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AppComponent {
   title = 'Login';
-  userName: string = '';
-  response: any;
-  constructor(private http: HttpClient) {
 
-  }
-  search(){
-    this.http.get('https://api.asgk-group.ru/test-auth-only' + this.userName)
-      .subscribe((response)=>{
-        this.response = response;
-        console.log(this.response);
-      })
-  }
 }
--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -2,26 +2,41 @@ import {Component, OnInit} from "@angular/core";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
-
+import {HttpClient} from "@angular/common/http";
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html'
 })
 export class AuthorizationComponent implements OnInit{
-  authorizationForm!: FormGroup
+
+  login: string = '';
+  password: string = '';
+  response: any;
   constructor(
+    private http: HttpClient,
     private router: Router,
     private authService: AuthService) {
   }
+  buttonEnter(){
+    this.http.post('https://api.asgk-group.ru/test-auth-only', {login:'test', password:'test'})
+      .subscribe((response) =>  {
+        this.response = response;
+        console.log(this.response);
+      })
+  }
+
+
+  authorizationForm!: FormGroup
+
   submitAuthorization(){
-this.authService.login(this.authorizationForm.value).subscribe({
-  next: () => this.router.navigate(['admin']),
-  error:(err) => alert(err.message)
-})
+    this.authService.login(this.authorizationForm.value).subscribe({
+    next: () => this.router.navigate(['admin']),
+    error:(err) => alert(err.message)
+  })
   }
   ngOnInit(): void{
  this.authorizationForm = new FormGroup({
-   'name': new FormControl(null, Validators.required),
+   'login': new FormControl(null, Validators.required),
    'password': new FormControl(null, Validators.required)
  })
   }
