cbab313de4855fca6cf8dc97f012dd4b667693f8
ViktorT777CT <p_viktor91@mail.ru>
2023-02-20 14:00:00 +0100

    Добавил классы для формы
    + добавили апи для карточек;
    + добавил цикл в ангуляр;

--- src/app/components/admin/components/home/models/home.ts
+++ src/app/components/admin/components/home/models/home.ts
@@ -1,4 +1,4 @@
 export interface IHome{
-  id: number
+  user_id: number
   title: string
 }
--- src/app/components/authorization/authorization.component.ts
+++ src/app/components/authorization/authorization.component.ts
@@ -3,25 +3,27 @@ import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {AuthService} from "../../services/auth.service";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+
 @Component({
   selector: 'app-authorization',
   templateUrl: './authorization.component.html'
 })
-export class AuthorizationComponent implements OnInit{
+export class AuthorizationComponent implements OnInit {
   title = 'Login';
 
   login: string = '';
   password: string = '';
   public response: any;
+
   constructor(
     private http: HttpClient,
     private router: Router,
     private authService: AuthService) {
   }
 
-  buttonEnter(){
-    this.http.post('https://api.asgk-group.ru/test-auth-only', {login:'test', password:'test'})
-      .subscribe((response) =>  {
+  buttonEnter() {
+    this.http.post('https://api.asgk-group.ru/test-auth-only', {login: 'test', password: 'test'})
+      .subscribe((response) => {
         this.response = response;
         console.log(this.response);
         localStorage.setItem('token', this.response.auth_token);
@@ -31,18 +33,17 @@ export class AuthorizationComponent implements OnInit{
 
   authorizationForm!: FormGroup
 
-  submitAuthorization(){
+  submitAuthorization() {
     this.authService.login(this.authorizationForm.value).subscribe({
-    next: () => this.router.navigate(['admin']),
-    error:(err) => alert(err.message)
-  })
-  }
-  ngOnInit(): void{
- this.authorizationForm = new FormGroup({
-   'login': new FormControl(null, Validators.required),
-   'password': new FormControl(null, Validators.required)
- })
+      next: () => this.router.navigate(['admin']),
+      error: (err) => alert(err.message)
+    })
   }
 
-
+  ngOnInit(): void {
+    this.authorizationForm = new FormGroup({
+      'login': new FormControl(null, Validators.required),
+      'password': new FormControl(null, Validators.required)
+    })
+  }
 }
